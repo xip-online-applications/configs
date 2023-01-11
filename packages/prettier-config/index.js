dd3c504e0137e666de6c78b4e622c9a06ec26371
Jeroen van Beek <losbeekos@gmail.com>
2023-01-11 13:59:40 +0100

    feat(package): add prettier-config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+};
