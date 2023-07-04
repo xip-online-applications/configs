86b51a1ee7dbbfd9066487b21600d733313d113b
Jeroen van Beek <losbeekos@gmail.com>
2023-07-04 13:13:40 +0200

    feat: set "no-unused-vars" to error

--- packages/eslint-config/shared/rules.js
+++ packages/eslint-config/shared/rules.js
@@ -1,5 +1,9 @@
 module.exports = {
   // Disabled these airbnb rules because TypeScript handles it properly
+  'no-unused-vars': [
+    'error',
+    { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+  ],
   'import/prefer-default-export': 0,
   'import/no-unresolved': 0,
   'import/extensions': 0,
